839ac82715e55b6742401cda13587ead33a15a1a
Manuel Castro <ingenierocastroud@gmail.com>
2020-11-24 14:33:12 -0500

    seccion 3 diseño

---
+++ src/app/pages/maindashboard/maindashboard.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-maindashboard',
+  templateUrl: './maindashboard.component.html',
+  styles: [
+  ]
+})
+export class MaindashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
